722f22e3b614e349a659158bec2ff05d1d4028f9
Mohammed Ali <alimohammed3731@gmail.com>
2026-08-14 23:10:29 +0530

    KClosestElements

---
+++ src/Day3/KClosestElements.java
@@ -0,0 +1,42 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class KClosestElements {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int k = sc.nextInt();
+        int target = sc.nextInt();
+
+        int low = 0;
+        int high = n - k;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (target - arr[mid] >
+                    arr[mid + k] - target) {
+
+                low = mid + 1;
+
+            } else {
+                high = mid;
+            }
+        }
+
+        for (int i = low; i < low + k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
