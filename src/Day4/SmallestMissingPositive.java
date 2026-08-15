7759b4cc6cfb962a6d403c53e75ccef6afd4e2d5
Mohammed Ali <alimohammed3731@gmail.com>
2026-08-15 22:50:13 +0530

    SmallestMissingPositive

---
+++ src/Day4/SmallestMissingPositive.java
@@ -0,0 +1,34 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class SmallestMissingPositive {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == mid + 1)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        System.out.println(
+                "Smallest Missing Positive = " + (low + 1));
+    }
+}
