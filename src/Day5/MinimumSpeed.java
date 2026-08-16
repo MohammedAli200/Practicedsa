e768face23fc6ad840305a35cd4349a97f7c02dd
Mohammed Ali <alimohammed3731@gmail.com>
2026-08-16 22:38:52 +0530

    MinimumSpeed

---
+++ src/Day5/MinimumSpeed.java
@@ -0,0 +1,56 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class MinimumSpeed {
+
+    static boolean canArrive(
+            int[] dist, int speed, double hour) {
+
+        double time = 0;
+
+        for (int i = 0; i < dist.length - 1; i++) {
+
+            time += Math.ceil(
+                    (double) dist[i] / speed);
+        }
+
+        time += (double) dist[dist.length - 1] / speed;
+
+        return time <= hour;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] dist = new int[n];
+
+        for (int i = 0; i < n; i++)
+            dist[i] = sc.nextInt();
+
+        double hour = sc.nextDouble();
+
+        int low = 1;
+        int high = 10000000;
+
+        int answer = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (canArrive(dist, mid, hour)) {
+                answer = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        System.out.println(
+                "Minimum Speed = " + answer);
+    }
+}
