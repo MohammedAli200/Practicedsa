392b093a8407beba5f22824d1307c401c566ebe0
Mohammed Ali <alimohammed3731@gmail.com>
2026-08-24 23:10:40 +0530

    IntegerSquareRoot

---
+++ src/Day13/IntegerSquareRoot.java
@@ -0,0 +1,37 @@
+package Day13;
+
+import java.util.Scanner;
+
+public class IntegerSquareRoot {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        if (n < 2) {
+            System.out.println("Square Root = " + n);
+            return;
+        }
+
+        long low = 1;
+        long high = n / 2;
+        long answer = 1;
+
+        while (low <= high) {
+
+            long mid = low + (high - low) / 2;
+
+            if (mid <= n / mid) {
+                answer = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println("Square Root = " + answer);
+    }
+}
