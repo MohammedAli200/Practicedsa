c8112e54a9547bc347735b1e2e7459f6380a2618
Mohammed Ali <alimohammed3731@gmail.com>
2026-08-24 23:09:08 +0530

    LastOccurrence

---
+++ src/Day13/LastOccurrence.java
@@ -0,0 +1,41 @@
+package Day13;
+
+import java.util.Scanner;
+
+public class LastOccurrence {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int target = sc.nextInt();
+
+        int low = 0;
+        int high = n - 1;
+        int answer = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                answer = mid;
+                low = mid + 1;
+            }
+            else if (arr[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println("Last Occurrence = " + answer);
+    }
+}
