dd1fa3d5602868ec40d247fb1d13941d268bd1c7
Mohammed Ali <alimohammed3731@gmail.com>
2026-08-26 22:49:25 +0530

    ShipPackages

---
+++ src/Day15/ShipPackages.java
@@ -0,0 +1,63 @@
+package Day15;
+
+import java.util.Scanner;
+
+public class ShipPackages {
+
+    static boolean canShip(
+            int[] weights, int days, int capacity) {
+
+        int current = 0;
+        int requiredDays = 1;
+
+        for (int weight : weights) {
+
+            if (current + weight > capacity) {
+                requiredDays++;
+                current = weight;
+            } else {
+                current += weight;
+            }
+        }
+
+        return requiredDays <= days;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] weights = new int[n];
+
+        int low = 0;
+        int high = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            weights[i] = sc.nextInt();
+
+            low = Math.max(low, weights[i]);
+            high += weights[i];
+        }
+
+        int days = sc.nextInt();
+
+        int answer = high;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (canShip(weights, days, mid)) {
+                answer = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
