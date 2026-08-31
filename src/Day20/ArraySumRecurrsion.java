d31b20b5d984f8c6a6fcac432a632f6a3b3d4250
Mohammed Ali <alimohammed3731@gmail.com>
2026-08-31 10:26:13 +0530

    ArraySumRecurrsion

--- src/Day20/ArraySumRecurrsion.java
+++ src/Day20/ArraySumRecurrsion.java
@@ -3,13 +3,11 @@ package Day20;
 public class ArraySumRecurrsion {
     static void main() {
         int[] arr={1,2,3,4,5,6};
-        int ans=0;
-        System.out.println(printSum(arr,arr.length-1,ans));
+        System.out.println(printSum(arr,arr.length-1));
 
     }
-    static int printSum(int[] arr,int i,int ans){
+    static int printSum(int[] arr,int i){
         if(i<0) return 0;
-        ans+=printSum(arr,i-1,ans);
-        return ans;
+        return arr[i]+printSum(arr,i-1);
     }
 }
