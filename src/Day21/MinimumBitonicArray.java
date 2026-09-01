a1be15477eb3be431bf092d2e82233c5c474293b
Mohammed Ali <alimohammed3731@gmail.com>
2026-09-01 23:05:47 +0530

    MinimumBitonicArray

---
+++ src/Day21/MinimumBitonicArray.java
@@ -0,0 +1,22 @@
+package Day21;
+
+import java.util.Scanner;
+
+public class MinimumBitonicArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int minimum = Math.min(arr[0], arr[n - 1]);
+
+        System.out.println("Minimum = " + minimum);
+    }
+}
