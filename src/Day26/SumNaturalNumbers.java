2bb7cf5c506a524b4fe381495a2b697072212ebc
Mohammed Ali <alimohammed3731@gmail.com>
2026-09-06 23:27:55 +0530

    SumNaturalNumbers

---
+++ src/Day26/SumNaturalNumbers.java
@@ -0,0 +1,12 @@
+package Day26;
+
+public class SumNaturalNumbers {
+    static int sum(int n) {
+        if (n == 0) return 0;
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(10)); // 55
+    }
+}
