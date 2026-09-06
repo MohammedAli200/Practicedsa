b8e571151d1a865ce5bb0c7cbfcb548e2d72246f
Mohammed Ali <alimohammed3731@gmail.com>
2026-09-06 23:30:27 +0530

    PrintNumbers

---
+++ src/Day26/PrintNumbers.java
@@ -0,0 +1,14 @@
+package Day26;
+
+public class PrintNumbers {
+    static void printNumbers(int n) {
+        if (n == 0) return;
+
+        printNumbers(n - 1);
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        printNumbers(5);
+    }
+}}
