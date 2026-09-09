a95383783319cf96f713fd32a69df281d56fcb80
Mohammed Ali <alimohammed3731@gmail.com>
2026-09-09 15:01:09 +0530

    LongestIncreasingSubsequence

---
+++ src/Day29/LongestIncreasingSubsequence.java
@@ -0,0 +1,29 @@
+package Day29;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+    public static int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1); // Every element is an LIS of length 1 by itself
+        int maxLen = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println("Length of LIS: " + lengthOfLIS(nums));
+    }
+}
