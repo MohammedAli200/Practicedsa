b765ac1121a3cf4776208c2990cd9c78d9f8514c
Mohammed Ali <alimohammed3731@gmail.com>
2026-09-13 22:49:09 +0530

    SubarraySumEqualsK

---
+++ src/Day32/SubarraySumEqualsK.java
@@ -0,0 +1,34 @@
+package Day32;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int currentSum = 0;
+        Map<Integer, Integer> prefixSumMap = new HashMap<>();
+
+        // Base case: prefix sum of 0 has occurred once
+        prefixSumMap.put(0, 1);
+
+        for (int num : nums) {
+            currentSum += num;
+
+            // If (currentSum - k) exists in map, we found valid subarray(s)
+            if (prefixSumMap.containsKey(currentSum - k)) {
+                count += prefixSumMap.get(currentSum - k);
+            }
+
+            prefixSumMap.put(currentSum, prefixSumMap.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1};
+        int k = 2;
+        System.out.println("Subarrays with sum K: " + subarraySum(nums, k)); // Output: 2
+    }
+}
