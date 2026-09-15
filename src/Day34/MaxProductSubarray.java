862acde05d521e023a2bb649b7ab3905635a64f2
Mohammed Ali <alimohammed3731@gmail.com>
2026-09-15 22:24:57 +0530

    MaxProductSubarray

---
+++ src/Day34/MaxProductSubarray.java
@@ -0,0 +1,30 @@
+package Day34;
+
+public class MaxProductSubarray {
+    public static int maxProduct(int[] nums) {
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        int result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // If current element is negative, swap min and max
+            if (nums[i] < 0) {
+                int temp = maxProd;
+                maxProd = minProd;
+                minProd = temp;
+            }
+
+            maxProd = Math.max(nums[i], maxProd * nums[i]);
+            minProd = Math.min(nums[i], minProd * nums[i]);
+
+            result = Math.max(result, maxProd);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, -2, 4};
+        System.out.println("Max Product Subarray: " + maxProduct(nums)); // Output: 6 ([2, 3])
+    }
+}
