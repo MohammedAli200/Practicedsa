5ee131f227956b687a5109da7bb58a436a7fe5c3
Mohammed Ali <alimohammed3731@gmail.com>
2026-09-18 22:59:47 +0530

    LongestPalindromicSubsequence

---
+++ src/Day36/LongestPalindromicSubsequence.java
@@ -0,0 +1,31 @@
+package Day36;
+
+public class LongestPalindromicSubsequence {
+    public static int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        // Base case: Single characters are palindromes of length 1
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+
+    public static void main(String[] args) {
+        String s = "bbbab";
+        System.out.println("Longest Palindromic Subsequence Length: " + longestPalindromeSubseq(s));
+    }
+}
